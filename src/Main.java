f7007bd4d33dccf62d7b15e55b427badaddb6523
Inácio salvador <inaciosalvador03@gmail.com>
2023-02-10 18:41:47 -0300

    Criação das classes: Curso e Mentoria. No programa principal foi criado 2 objetos do tipo curso e 1 objeto do tipo mentoria. Logo abaixo foi feita as impressoes para demonstração.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Curso curso1 = new Curso(); //objeto1
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso(); // objeto2
+        curso1.setTitulo("Curso php");
+        curso1.setDescricao("Descrição curso php");
+        curso1.setCargaHoraria(22);
+
+        Mentoria mentoria = new Mentoria(); //objeto do tipo mentoria
+        mentoria.setTitulo("Mentoria java");
+        mentoria.setDescricao("Descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
     }
 }
